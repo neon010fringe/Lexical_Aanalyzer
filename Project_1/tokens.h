9659df49505907a15b5ed9456dce08ccfa271c42
James Deady <james.t.deady@gmail.com>
2025-06-26 10:56:11 -0700

    Initial commit of CMSC 430 Lexical Analyzer project

---
+++ Project_1/tokens.h
@@ -0,0 +1,12 @@
+// CMSC 430 Compiler Theory and Design
+// Project 1 Skeleton
+// UMGC CITE
+// Summer 2023
+
+// This file contains the enumerated type definition for tokens
+
+enum Tokens { ADDOP = 256, MULOP, ANDOP, RELOP, ARROW, BEGIN_, CASE, CHARACTER, END, 
+			ENDSWITCH, FUNCTION, INTEGER, IS, LIST, OF, OTHERS, RETURNS, SWITCH, WHEN,
+  			IDENTIFIER, INT_LITERAL, HEXINT, CHAR_LITERAL, REAL, IF, THEN, ELSE, ELSIF, ENDIF,
+  			FOLD, LEFT, RIGHT, ENDFOLD, ASSIGNOP, OROP, NOTOP, REMOP, EXPOP, REGOP, 
+  			NEGOP, REALIT };
